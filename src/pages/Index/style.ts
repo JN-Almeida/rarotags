27841a4a20e3f891af5464d96d48571c8ffa0d5e
J_Almeida <jcalmeida.jonathan@gmail.com>
2021-07-19 12:47:04 -0300

    styled in mobile

--- src/pages/Index/style.ts
+++ src/pages/Index/style.ts
@@ -6,6 +6,9 @@ export const Container = styled.div`
     border-radius: 5px;
     padding: 20px;
     box-shadow: 0 0 8px #ccc;
+    @media (max-width: 680px){
+        width: 90vw;
+    }
 
     span{
         margin: 10px;
